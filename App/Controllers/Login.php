3f1c2850f2f9d99e70ecb55f6aaa3175600fc08b
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-03-05 21:31:14 +0100

    Create login controller and action

---
+++ App/Controllers/Login.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+class Login extends \Core\Controller{
+
+    public function newAction(){
+        View::renderTemplate('Login/new.html');
+
+    }
+
+    public function createAction(){
+        var_dump($_POST);
+    }
+}
+
+?>
