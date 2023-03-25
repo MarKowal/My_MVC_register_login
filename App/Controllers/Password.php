b4a9099c1631507c581e4fdf25c8ec172bfa7c00
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-03-25 21:49:00 +0100

    Add form for password reset

---
+++ App/Controllers/Password.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\Controller;
+use \Core\View;
+
+class Password extends Controller{
+
+    public function forgotAction(){
+        View::renderTemplate('Password/forgot.html');
+    }
+}
+
+
+
+
+?>
