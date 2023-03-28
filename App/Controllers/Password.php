eae3fc8397495a865807997c4947ddbbcd9ca28a
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-03-28 20:50:35 +0200

    Create password reset form

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -26,7 +26,12 @@ class Password extends Controller{
 
         $user = User::findByPasswordReset($token);
 
-        var_dump($user);
+        if($user){
+            View::renderTemplate('Password/reset.html');
+
+        } else {
+            echo 'password reset token invalid';
+        }
     }
 
 }
