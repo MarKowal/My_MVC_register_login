070ebe8e24e5219a6266eee2840bb4506184b596
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-03-28 21:04:27 +0200

    Create app.js for repeated JS code

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+//custom validation method for password in Signup and Password Reset:
+$.validator.addMethod('validPassword',
+    function(value, element, param) {
+        if (value != '') {
+            if (value.match(/.*[a-z]+.*/i) == null) {
+                return false;
+            }
+            if (value.match(/.*\d+.*/) == null) {
+                return false;
+            }
+        }
+        return true;
+    },
+    'Must contain at least one letter and one number'
+);
