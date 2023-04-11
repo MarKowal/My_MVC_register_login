223862a0473742589c5d75e79901b74e552ac747
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-04-11 20:12:16 +0200

    Fix duplicated code

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -10,30 +10,36 @@ use \App\Flash;
 //żeby user był już zalogowany to trzeba dziedziczyć po Athenticated
 class Profile extends Authenticated{
 
+    protected function before(){
+        parent::before();
+        $this->user = Auth::getUser();
+    }
+
     public function showAction(){
         View::renderTemplate('Profile/show.html', [
             //pobieram dane usera z Auth i przekazuję do view:
-            'user' => Auth::getUser()
+            //'user' => Auth::getUser()
+            'user' => $this->user
         ]);
     }
 
     public function editAction(){
         View::renderTemplate('Profile/edit.html', [
-            'user' => Auth::getUser()
+            'user' => $this->user
         ]);
     }
 
     public function updateAction(){
         //stawiam obiekt User żeby wykonywać jego metody
-        $user = Auth::getUser();
+        //$user = Auth::getUser();
 
-        if($user->updateProfile($_POST)){
+        if($this->user->updateProfile($_POST)){
             Flash::addMessages('Changes saved');
             $this->redirect('/profile/show');
             
         } else {
             View::renderTemplate('Profile/edit.html', [
-                'user' => $user
+                'user' => $this->user
             ]);
         }
     }
